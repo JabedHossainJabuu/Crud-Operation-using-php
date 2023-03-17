83357d6906075606ca476f7e435d469d0b7e2790
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-03-18 04:23:20 +0600

    first

---
+++ display.php
@@ -0,0 +1,91 @@
+<?php 
+include('connect.php');
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud Operation</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <button class="btn btn-primary my-5">
+            <a href="user.php" class="text-light">Add User</a>
+        </button>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">SI no</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Operation</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+
+                $sql= "SELECT * FROM `crud`";
+                $result = mysqli_query($con,$sql);
+
+                if($result){
+                    while($row=mysqli_fetch_assoc($result)){
+                        $id=$row['id'];
+                        $name=$row['name'];
+                        $email=$row['email'];
+                        $mobile=$row['mobile'];
+                        $password=$row['password'];
+
+                        echo '<tr>
+                        <th scope="row">'.$id.'</th>
+                        <td>'.$name.'</td>
+                        <td>'.$email.'</td>
+                        <td>'.$mobile.'</td>
+                        <td>'.$password.'</td>
+                      </tr>';
+                    }
+                }
+
+
+                ?>
+                <!-- <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr> -->
+            </tbody>
+        </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,80 @@
+<?php 
+
+
+include 'connect.php';
+
+if(isset($_POST ['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO `crud`(name,email,mobile,password)
+    values('$name','$email','$mobile','$password')";
+
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        echo "Data inserted successffully!";
+    }else {
+        die(mysqli_errno($con));
+    }
+}
+
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>User</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <form method="POST">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" placeholder="Enter your name" name="name" id="name" class='form-control' autocomplete="off">
+            </div>
+            
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" placeholder="Enter your email" name="email" id="email" class='form-control' autocomplete="off">
+            </div>
+
+            <div class="form-group">
+                <label for="mobile">Mobile</label>
+                <input type="text" placeholder="Enter your mobile number" name="mobile" id="mobile" class='form-control' autocomplete="off">
+            </div>
+            
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" placeholder="Enter your password" name="password" id="password" class='form-control'>
+            </div>
+
+            <button type="submit" class="btn btn-primary"
+            name="submit">Submit</button>
+        </form>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
